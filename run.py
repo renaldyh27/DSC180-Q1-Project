24e6f15d4fe6fd24cb16de7e1e8d90b1171f6061
emersonchao <echao011@gmail.com>
2022-11-22 18:00:40 -0800

    add run.py

---
+++ run.py
@@ -0,0 +1,8 @@
+## NECESSARY IMPORTS
+
+if __name__ == "__main__":
+    #TODO: load data, clean, save processed data
+    #TODO: loop for cross validation
+    #TODO: build models, predict, and score
+    #TODO: save plotting data, generate plot
+    #TODO: other outputs (final models, scores, etc.)
