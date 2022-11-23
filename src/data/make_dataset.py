0c2c471fb742ee12b3a59ab5b8c9ddbcbc7f4925
Renaldy Herlim <renaldyh27@gmail.com>
2022-11-23 00:02:57 -0800

    Started run.py code and other supporting scripts in data and models dir
    Started to write the functions that will be used in the run.py file and started writing the run.py code structure and imports

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,3 +6,28 @@ def read_fungi_data(path):
 
 def read_tcga_abbrev(path):
     return pd.read_csv(path, index_col='dz')
+
+def proces_metadata(metadata_file):
+    '''Takes in raw metadata file and outputs 2 things: extract the relevant sample IDs and extract one hot encoding of the Cancer Types'''
+    fungi_metadata = metadata_file
+    
+    fungi_metadata_cols = ['disease_type','sample_type'] 
+    fungi_metadata = fungi_metadata[fungi_metadata_cols] #grab needed columns
+    fungi_metadata = fungi_metadata[fungi_metadata['sample_type'] == 'Primary Tumor'] #filter only Primary Tumors
+
+    min_count = 20 #drop cancers with fewer than 20 samples
+    fungi_metadata = fungi_metadata[fungi_metadata['disease_type'].map(fungi_metadata['disease_type'].value_counts()) > min_count]
+
+    sample_ids = fungi_metadata.index #relevant sampleID's after final cleaning
+    #TODO Create sample relevant sample IDs tsvs in interim data
+
+    cancer_types = pd.get_dummies(fungi_metadata['disease_type']) #one hot encode disease types
+    #TODO Create relevant cancer types encoding tsvs in interim data
+
+    return 
+
+def process_fungi_data(data, sample_ids):
+    '''Takes in the raw fungi data and filter for relevant samples and creates the dataset for interim data'''
+    filtered_dataset = data.filter(items = sample_ids, axis=0) #filter feature tables for relevant samples
+    #TODO Create dataset for interim data
+    return filtered_dataset
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,3 +1,24 @@
 from sklearn.model_selection import StratifiedKFold
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score
+
+## TODO Py script to train and output model ## 
+def train_models(datasets, cancer_types):
+    '''Take in as input the cleaned datasets of the features(X) and the one-hot encoded cancer types/targets(Y)
+       then perform 10-fold validation split and use them to train the model.
+       
+       Output: Trained Model for all the datasets
+       
+    '''
+    models = []
+    #Get the cleaned dataset to use for Model Training
+    
+        #Output cleaned datasets features (Predictors/X) & cancer types (Prediction/Y) dataset to use for model training
+
+
+    #Perform the CV splits 
+
+    #Train model for each cancer type
+
+    #Return the trained model for the specified dataset
+    return models
