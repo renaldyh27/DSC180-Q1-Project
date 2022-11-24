aebc22dc19667ac3eefaf239d76006271e1d1bc6
Amando Jimenez <57554350+Amandoj@users.noreply.github.com>
2022-11-23 18:03:30 -0800

    creating data for ma5

--- src/data/test_data_generator.py
+++ src/data/test_data_generator.py
@@ -17,19 +17,16 @@ taxa_columns = ['Candida_glabrata', 'Candida_tropicalis', 'Debaryomyces_hansenii
            'Agaricus_bisporus', 'Candida_orthopsilosis', 'Malassezia_sympodialis',
            'Cyphellophora_europaea', 'Wallemia_ichthyophaga',
            'Pseudozyma_hubeiensis', 'Bipolaris_zeicola'] #subset of all taxas in raw data
-
-def generate_test_data(n_samples=n_samples):
     
-    test_metadata = pd.DataFrame(np.random.choice(cancers,size=(n_samples, 1)), columns=['disease_type'])
-    test_metadata['sampleid'] = np.arange(1, n_samples+1)
-    test_metadata['sample_type'] = 'Primary Tumor'
+test_metadata = pd.DataFrame(np.random.choice(cancers,size=(n_samples, 1)), columns=['disease_type'])
+test_metadata['sampleid'] = np.arange(1, n_samples+1)
+test_metadata['sample_type'] = 'Primary Tumor'
 
-    test_fungi_data = pd.DataFrame(np.random.randint(0, 300, size=(n_samples,len(taxa_columns))), columns=taxa_columns)
-    test_fungi_data['sampleid'] = np.arange(1, n_samples+1)
+test_fungi_data = pd.DataFrame(np.random.randint(0, 300, size=(n_samples,len(taxa_columns))), columns=taxa_columns)
+test_fungi_data['sampleid'] = np.arange(1, n_samples+1)
 
-    paths = ('test/test_metadata.tsv', 'test/test_fungi_data.tsv')
+paths = ('test/test_metadata.tsv', 'test/test_fungi_data.tsv')
+
+test_metadata.to_csv(paths[0], sep="\t", index=False)
+test_fungi_data.to_csv(paths[1], sep="\t", index=False)
     
-    test_metadata.to_csv(paths[0], sep="\t", index=False)
-    test_fungi_data.to_csv(paths[1], sep="\t", index=False)
-    
-    return paths
